b2377fe672707bd43ab15beb67de88818c9f13ee
Jay Frost <jayfrost321@gmail.com>
2020-09-07 14:36:48 +1200

    Added user & share CRUDs. Also added some comment notes.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//importing modules and initial setup
 var express = require('express')
 var bodyParser = require('body-parser')
 var logger = require('morgan')
@@ -7,7 +8,6 @@ var mongoose = require('mongoose')
 var Share = require('./share-model')
 var User = require('./user-model')
 var Comment = require('./comment-model')
-const { populate } = require('./share-model')
 
 var app = express()
 app.use(cors())
@@ -23,22 +23,7 @@ database.once('open', () => console.log('Connected'))
 database.on('error', () => console.log('Error'))
 var router = express.Router()
 
-
-router.get('/shares', (req, res) => {
-    Share.find()
-    .populate('user')
-    .then((share) => {
-      res.json(share);
-    })
-})
-
-router.get('/shares/:id', (req, res) => {
-    Share.findOne({id:req.params.id})
-	.then((share) => {
-	    res.json(share)
- 	})
-})
-
+//Share C.R.U.D
 router.post('/shares', (req, res) => {
     var share = new Share()
     share.id = Date.now()
@@ -49,26 +34,44 @@ router.post('/shares', (req, res) => {
     share.save()
     .then((share) => {
         res.json(share)
-    })
+    }) //create new share to database
 })
-
-//TODO: Update share
-
-
-
-//TODO: Delete Share
-
-
-//TODO: COMMENT GET
-router.get('/comments', (req, res) => {
-    Comment.find()
+router.get('/shares', (req, res) => {
+    Share.find()
     .populate('user')
-    .populate('share')
-    .then((comment) => {
-      res.json(comment);
-    })
+    .then((share) => {
+      res.json(share);
+    }) //Read all shares
+})
+router.get('/shares/:id', (req, res) => {
+    Share.findOne({id:req.params.id})
+	.then((share) => {
+	    res.json(share)
+ 	}) //read individual share
+})
+router.put('/shares/:id', (req, res) => {
+
+	Share.findOne({id:req.params.id})
+	.then((share) => {
+		var data = req.body
+		Object.assign(share,data)
+		return share.save()	
+	})
+	.then((artist) => {
+		 res.json(artist)
+	}) //update existing share data
+
+})
+router.delete('/shares/:id', (req, res) => {
+
+	Share.deleteOne({id:req.params.id})
+	.then(() => {
+		res.json('deleted')
+	}) // delete share
+	
 })
 
+//Comment C.R.U.D
 router.post('/comments', (req, res) => {
     var comment = new Comment()
     comment.id = Date.now()
@@ -79,19 +82,24 @@ router.post('/comments', (req, res) => {
     comment.save()
     .then((comment) => {
         res.json(comment)
-    })
+    }) // Create new comment
 })
-
-//TODO: Comment By ID 
+router.get('/comments', (req, res) => {
+    Comment.find()
+    .populate('user')
+    .populate('share')
+    .then((comment) => {
+      res.json(comment);
+    }) // read all comments
+})
 router.get('/comments/:id', (req, res) => {
     Comment.findOne({id:req.params.id})
     .populate('user')
     .populate('share')
 	.then((comment) => {
 	    res.json(comment)
- 	})
+ 	}) // read individual comment
 })
-//TODO: Update Comments 
 router.put('/comments/:id', (req, res) => {
     Comment.findOne({id:req.params.id})
     .then((comment) => {
@@ -101,34 +109,64 @@ router.put('/comments/:id', (req, res) => {
     })
     .then((comment) => {
         return res.json(comment)
-    })
+    }) // update comment data
 })
-//TODO: Delete Comment
 router.delete('/comments/:id', (req, res) => {
     Comment.deleteOne({id: req.params.id})
     .then(() =>  {
         res.json('deleted')
-    })
+    }) // delete a comment 
 })
+//abc
+//User C.R.U.D
+router.post('/users', (req, res) => {
 
+    var user = new User()
+    user.id = Date.now()
+    
+    var data = req.body
+    Object.assign(user,data)
+    user.save()
+    .then((user) => {
+            res.json(user)
+        }) // create a new user
+
+})
 router.get('/users', (req, res) => {
     User.find()
     .then((user) => {
       res.json(user);
-    })
+    }) //read all users
 })
-
 router.get('/users/:id', (req, res) => {
     User.findOne({id:req.params.id})
 	.then((user) => {
 	    res.json(user)
- 	})
+ 	}) //read individual user
+})
+router.put('/users/:id', (req, res) => {
+
+	User.findOne({id:req.params.id})
+	.then((user) => {
+		var data = req.body
+		Object.assign(user,data)
+		return user.save()	
+	})
+	.then((user) => {
+		 res.json(user)
+	}) //update user details
+
+})
+router.delete('/users/:id', (req, res) => {
+
+	User.deleteOne({id:req.params.id})
+	.then(() => {
+		res.json('deleted')
+	}) // delete a user
+	
 })
 
-//TODO: UPDATE USER
-
-//TODO: DELETE USER 
-
+//Setup port and routes
 app.use('/api', router)
 const Port = 4020;
 app.listen(Port, () => console.log('Listening Port: '+Port))
